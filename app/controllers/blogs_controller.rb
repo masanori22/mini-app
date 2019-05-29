84d1c2ad9d3fc3f09b3c0356a888a2479150f243
masanori22 <aga.in@icloud.com>
2019-05-29 15:45:31 +0900

    発展機能１

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,9 @@
 class BlogsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.order("created_at ASC").page(params[:page]).per(5)
   end
 
   def new
@@ -13,5 +16,9 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?  
   end
 end
